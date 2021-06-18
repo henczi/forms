2ef0ec0077b73b89cb5e2e9774d2fb3ccca174f7
Henczidai Bálint <henczidaibalint@gmail.com>
2021-06-18 18:53:22 +0200

    meghatalmazas - deno test

---
+++ mod.ts
@@ -0,0 +1,62 @@
+const index = (items: { name: string, path: string }[]) => `
+<html>
+    <body>
+        <h1>Forms</h1>
+        <ul>
+            ${items.map(x => `<li><a href="${x.path}">${x.name}</a></li>`).join('\n')}
+        </ul>
+    </body>
+</html>
+`;
+
+const e404 = () => `
+<html>
+    <body>
+        <h1 style="text-align: center" >Opps - 404</h1>
+    </body>
+</html>
+`;
+
+const forms = [
+    { name: 'Meghatalmazás', path: '/meghatalmazas.html' }
+];
+
+
+async function handleRequest(request: Request) {
+    const { pathname } = new URL(request.url);
+
+    if (pathname === '/') {
+        return new Response(
+            index(forms),
+            {
+                headers: {
+                    "content-type": "text/html; charset=utf-8",
+                },
+            },
+        );
+    }
+
+    for (const form of forms) {
+        if (pathname === form.path) {
+            const formUrl = new URL(form.path.substring(1), import.meta.url);
+            const response = await fetch(formUrl);
+            const headers = new Headers(response.headers);
+            headers.set("content-type", "text/html; charset=utf-8");
+            return new Response(response.body, { ...response, headers });
+        }
+    }
+
+
+    return new Response(
+        e404(),
+        {
+            headers: {
+                "content-type": "text/html; charset=utf-8",
+            },
+        },
+    );
+}
+
+addEventListener("fetch", (event: FetchEvent) => {
+    event.respondWith(handleRequest(event.request));
+});
